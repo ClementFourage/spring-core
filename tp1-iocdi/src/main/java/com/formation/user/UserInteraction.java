8551838128d548940cf3bd432c27e90845c71066
Clement Fourage <clement-fourage@hotmail.fr>
2018-08-30 12:08:55 +0200

    TP 01 Spring

---
+++ tp1-iocdi/src/main/java/com/formation/user/UserInteraction.java
@@ -0,0 +1,37 @@
+package com.formation.user;
+
+import com.formation.service.FeedbackFactory;
+import java.text.MessageFormat;
+
+/**
+ * Class to make interactions with the user
+ */
+public class UserInteraction {
+	private FeedbackFactory factory;
+
+	public UserInteraction() {
+		// feedback = factory.getFeedback();
+	}
+
+	/**
+	 * To say hello to the user named <code>name</code>
+	 *
+	 * @param name
+	 */
+	public void sayHello(String name) {
+		factory.getFeedback().say(MessageFormat.format("Hello {0} !", name));
+	}
+
+	/**
+	 * To say goodbye to the user named <code>name</code>
+	 *
+	 * @param name
+	 */
+	public void sayGoodBye(String name) {
+		factory.getFeedback().say(MessageFormat.format("Goodbye {0} !", name));
+	}
+
+	public void setFactory(FeedbackFactory factory) {
+		this.factory = factory;
+	}
+}
